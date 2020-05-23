6884ceb86f0bbdf6240d5ecb557a805311878572
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-05-23 12:12:31 -0500

    agregado routesManager al proyecto

---
+++ lib/routesManager.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RoutesManager {
+  static const _routeNameFirst = '/myHomePage';
+  static const _routeNameSecond = '/passwordMenu';
+  static const _routeNameThird = '/cardGenerator';
+
+  String routeNameFirst() => _routeNameFirst;
+  String routeNameSecond() => _routeNameSecond;
+  String routeNameThird() => _routeNameThird;
+}
