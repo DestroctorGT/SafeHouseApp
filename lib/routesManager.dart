57cb04456aa7cc68142f0b112ea1ba3585b2cdb3
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-05-26 16:20:02 -0500

    Agregados comentarios de referencia al codigo

--- lib/routesManager.dart
+++ lib/routesManager.dart
@@ -1,8 +1,10 @@
 class RoutesManager {
+  //Variables static encargadas de almacenar el nombre de las rutas.
   static const _routeNameFirst = '/MyHomePage';
   static const _routeNameSecond = '/PasswordMenu';
   static const _routeNameThird = '/CardGenerator';
 
+  //Metodos encargados de retornar el nombre de las rutas.
   String routeNameFirst() => _routeNameFirst;
   String routeNameSecond() => _routeNameSecond;
   String routeNameThird() => _routeNameThird;
