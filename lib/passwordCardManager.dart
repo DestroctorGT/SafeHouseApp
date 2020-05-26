3a4d52c6814ea9cd08f90a97b889fe0b060eba14
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-05-26 16:19:21 -0500

    Agregado comentarios de referencia al codigo

--- lib/passwordCardManager.dart
+++ lib/passwordCardManager.dart
@@ -1,36 +1,13 @@
 import 'package:flutter/material.dart';
-import 'routesManager.dart';
-import 'package:get/get.dart';
 
 class PasswordCardManager {
-  List<Card> _passwordCards = [
-    Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            height: 20,
-          ),
-          new Image.asset(
-            'images/twitter logo.png',
-            width: 90,
-            height: 90,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text('Usuario'),
-          SizedBox(
-            height: 10,
-          ),
-          Text('Contraseña'),
-        ],
-      ),
-    )
-  ];
+  //Lista de Cards encargada de agregar todas las cuentas que cree el usuario.
+  List<Card> _passwordCards = [];
 
+  //Metodo encargado de retornar la lista de cards.
   List returnList() => _passwordCards;
 
+  //Metodo encargado de agregar nuevas cards a la lista.
   void addCards({String image, String user}) {
     _passwordCards.add(Card(
       child: Column(
