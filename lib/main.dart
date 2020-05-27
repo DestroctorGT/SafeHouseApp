737d66bc68308ba06501fd487ab89df373efaab5
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-05-27 11:38:52 -0500

    Correciones Menores

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:safehouseapp/passwordCardManager.dart';
-import 'accountImagesManager.dart';
 import 'routesManager.dart';
 import 'package:get/get.dart';
+import 'passwordMenu.dart';
+import 'cardGeneratorMenu.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       theme: ThemeData.dark(),
-      initialRoute: routes.routeNameFirst(),
+      home: MyHomePage(),
       namedRoutes: {
         routes.routeNameFirst(): GetRoute(page: MyHomePage()),
         routes.routeNameSecond(): GetRoute(page: PasswordMenu()),
@@ -57,115 +58,6 @@ class _MyHomePageState extends State<MyHomePage> {
           Get.toNamed(routes.routeNameSecond());
         },
         child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class PasswordMenu extends StatelessWidget {
-  AccountImagesManager iManager = AccountImagesManager();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('SafeHouse')),
-      ),
-      body: GridView.count(
-        padding: EdgeInsets.all(12),
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        crossAxisCount: 2,
-        children: iManager.imagesReturn(),
-      ),
-    );
-  }
-}
-
-class CardGenerator extends StatefulWidget {
-  @override
-  _CardGeneratorState createState() => _CardGeneratorState();
-}
-
-class _CardGeneratorState extends State<CardGenerator> {
-  final textFieldController = TextEditingController();
-
-  @override
-  void dispose() {
-    textFieldController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('SafeHouse')),
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 50,
-            ),
-            Flexible(
-              child: Container(
-                child: new Image.asset(
-                  Get.arguments,
-                  width: 150,
-                  height: 150,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Flexible(
-              child: TextField(
-                controller: textFieldController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(20),
-                  filled: true,
-                  labelText: 'User',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Flexible(
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(20),
-                  filled: true,
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 70,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                RaisedButton(
-                  padding: EdgeInsets.all(15),
-                  onPressed: () {},
-                  child: Text('Create Password'),
-                ),
-                RaisedButton(
-                  padding: EdgeInsets.all(15),
-                  onPressed: () {
-                    myPasswordCards.addCards(
-                        image: Get.arguments, user: textFieldController.text);
-                    Get.offAllNamed(routes.routeNameFirst());
-                  },
-                  child: Text('OK'),
-                )
-              ],
-            )
-          ],
-        ),
       ),
     );
   }
