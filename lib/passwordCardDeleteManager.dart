ab120ba663184b0d2db40a2329da9d49614d049f
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-06-04 11:51:40 -0500

    Correciones Menores

---
+++ lib/passwordCardDeleteManager.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PasswordCardDeleteManager extends StatefulWidget {
+  @override
+  _PasswordCardDeleteManagerState createState() =>
+      _PasswordCardDeleteManagerState();
+}
+
+class _PasswordCardDeleteManagerState extends State<PasswordCardDeleteManager> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Safehouse'),
+        ),
+      ),
+    );
+  }
+}
