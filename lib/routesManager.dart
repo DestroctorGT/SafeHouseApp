94c3bd655855698033754e526412ddd361256380
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-06-04 11:44:15 -0500

    Agregado un nuevo string al codigo

--- lib/routesManager.dart
+++ lib/routesManager.dart
@@ -3,9 +3,11 @@ class RoutesManager {
   static const _routeNameFirst = '/MyHomePage';
   static const _routeNameSecond = '/PasswordMenu';
   static const _routeNameThird = '/CardGenerator';
+  static const _routeNameFourth = '/PasswordCardDeleteManager';
 
   //Metodos encargados de retornar el nombre de las rutas.
   String routeNameFirst() => _routeNameFirst;
   String routeNameSecond() => _routeNameSecond;
   String routeNameThird() => _routeNameThird;
+  String routeNameFourth() => _routeNameFourth;
 }
