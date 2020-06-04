1e7f55c051279d984030df4bdfae448a435ddcab
DestroctorGT <57532021+DestroctorGT@users.noreply.github.com>
2020-06-04 12:05:11 -0500

    Correciones Menores

--- lib/passwordCardDeleteManager.dart
+++ lib/passwordCardDeleteManager.dart
@@ -15,6 +15,74 @@ class _PasswordCardDeleteManagerState extends State<PasswordCardDeleteManager> {
           child: Text('Safehouse'),
         ),
       ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: TextFormField(
+                // ignore: missing_return
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Enter your username';
+                  }
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20),
+                  filled: true,
+                  labelText: 'User',
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Flexible(
+            child: Container(
+              margin: EdgeInsets.all(20),
+              child: TextFormField(
+                readOnly: true,
+                // ignore: missing_return
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'No password';
+                  }
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(20),
+                  filled: true,
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(20),
+                child: RaisedButton(
+                  color: Colors.blueAccent,
+                  padding: EdgeInsets.all(15),
+                  onPressed: () {},
+                  child: Text('Create Password'),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                child: RaisedButton(
+                  color: Colors.blueAccent,
+                  padding: EdgeInsets.all(15),
+                  onPressed: () {},
+                  child: Text('OK'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
